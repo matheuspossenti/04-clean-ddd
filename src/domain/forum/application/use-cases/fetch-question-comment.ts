5dad4c7418062a34e1b06ee4e25ff0d000ea09d7
Matheus <matheus.possenti24@gmail.com>
2024-11-04 15:02:36 -0300

    delete answer comments use case and tests and fetch question comments use case and tests

---
+++ src/domain/forum/application/use-cases/fetch-question-comment.ts
@@ -0,0 +1,29 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+interface FetchQuestionCommentsUseCaseRequest {
+  page: number
+  questionId: string
+}
+
+interface FetchQuestionCommentsUseCaseResponse {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(
+    private questionsCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    page,
+    questionId,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+    const questionComments =
+      await this.questionsCommentsRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+
+    return { questionComments }
+  }
+}
